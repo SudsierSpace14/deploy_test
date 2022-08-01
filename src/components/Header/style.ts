d72fc16499ff0f44ff1168b763f87c38664bdd44
unknown <sudsier.gabriel@gmail.com>
2022-08-01 20:03:08 -0300

    Initialised app and made header

---
+++ src/components/Header/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const HeaderStyle = styled.header`
+    background-image: url("https://static.wixstatic.com/media/0e657f_51783003159a4c1da3f0b4b992dc5f82~mv2.jpg/v1/fill/w_1423,h_221,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/0e657f_51783003159a4c1da3f0b4b992dc5f82~mv2.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+
+    img{
+        width: 300px;
+        padding: 25px;
+    }
+`
+
+export const RowHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
---
+++ src/components/style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+export const MediumContainer = styled.div`
+    max-width: 1300px;
+    height: 100%;
+    margin: 0 auto;
+`
+export const SmallContainer = styled.div`
+    max-width: 1000px;
+    margin: 0 auto;
+`
